b15d3ecebc19f7c527d486a8cff7ad29a31e40f1
Christian McCabe <christian.mccabe89@gmail.com>
2022-09-13 12:39:27 -0600

    Add reassignments to string variables and puts statements

--- variables.rb
+++ variables.rb
@@ -6,3 +6,8 @@ cost_cats = 223.32
 cost_dogs = 367.01
 cats_cute = false
 dogs_cute = true
+cats = "The cats in my house are menaces"
+dogs = "The dogs are very cute"
+puts cats
+puts num_cats
+puts dogs_cute
